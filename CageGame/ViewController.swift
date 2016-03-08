5cc7ce29d92e0b41c488de28c3f682ecfd987721
Jitrixis <170535@supinfo.com>
2016-03-08 19:58:31 +0100

    Init
    Single View Project

---
+++ CageGame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CageGame
+//
+//  Created by Julien Gardet on 08/03/2016.
+//  Copyright © 2016 Julien Gardet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
